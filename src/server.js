7a626698cd579bf5f93e1cd916c2c83cb5e86299
Victor Klimov <4klimov@gmail.com>
2021-02-28 15:06:35 +0300

    feat: Add DB connection and model

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+const { connectDB } = require('./common/database');
 const app = require('./app');
 const { PORT } = require('./common/config');
 
-app.listen(PORT, () => {
-  console.log(`Express server is listening on port ${PORT}`);
-});
+(async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Express server is listening on port ${PORT}`);
+  });
+})();
