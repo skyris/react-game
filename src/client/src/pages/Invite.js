0dd335464112bcab0bcbaf18887ad28ce9b4f56a
Victor Klimov <4klimov@gmail.com>
2021-03-03 17:54:28 +0300

    feat: Invite page

---
+++ src/client/src/pages/Invite.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Invite() {
+  return (
+    <div className="invite">
+      <p>Hello to the Game</p>
+      <button
+        className="button"
+      >
+        Start the game
+      </button>
+    </div>
+  );
+}
+
+export default Invite;
