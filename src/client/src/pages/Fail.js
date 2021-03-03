386440f9fae7c18d37934fa8fcef913b776490a9
Victor Klimov <4klimov@gmail.com>
2021-03-03 17:56:54 +0300

    feat: Fail page

---
+++ src/client/src/pages/Fail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Fail(props) {
+  return (
+    <div className="fail">
+      <p>You fail ;-(</p>
+      <button
+          className="button"
+      >
+        Play one more time
+      </button>
+    </div>
+  )
+}
+
+export default Fail;
